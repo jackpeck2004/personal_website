f1099dbaf1fe59ee6254483e437a1a910377fb3e
Giacomo <giacomo.pasin@gmail.coom>
2020-06-28 15:30:30 +0200

    began to add sidebar

---
+++ src/pages/test.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Sidebar = () => {
+  return (
+    <div className=''>
+      <h1>SideBar</h1>
+    </div>
+  );
+}
+
+const Test = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+
+  )
+}
+
+export default Test
