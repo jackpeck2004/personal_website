ed6101f9778318da55972dd599bbd07ff896883c
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-06-29 14:51:02 +0200

    finsihed mod of sidebar function

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,18 +2,25 @@ import { Link } from 'gatsby'
 import scrollTo from "gatsby-plugin-smoothscroll"
 import React from 'react'
 import '../styles/Menu.scss'
+import { openSidebar, sidebarState } from "./Sidebar"
+
+const st = (target: string) => {
+  scrollTo(target)
+  openSidebar(!sidebarState)
+
+}
 
 export const Links = () => (
   <nav>
     <ul className="nav__links">
       <li>
-        <a className="nav_link" onClick={() => scrollTo(".Home")}>Home</a>
+        <a className="nav_link" onClick={() => st(".Home")}>Home</a>
       </li>
       <li>
-        <a className="nav_link" onClick={() => scrollTo("#projects")}>About</a>
+        <a className="nav_link" onClick={() => st("#projects")}>About</a>
       </li>
       <li>
-        <a className="nav_link" onClick={() => scrollTo(".projects")}>Projects</a>
+        <a className="nav_link" onClick={() => st(".projects")}>Projects</a>
       </li>
     </ul>
   </nav>
