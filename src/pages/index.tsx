0eff15a9f68c8ffdcc1eb2e97f94dfd9358b85fe
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-07-03 14:34:19 +0200

    adding Menu again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import Media from "react-media"
 import ContactForm from "../components/Contact"
 import Home from "../components/Home"
 import Layout from "../components/Layout"
+import Menu from "../components/Menu"
 import Projects from "../components/Projects"
 import Sidebar, { openSidebar, sidebarState } from "../components/Sidebar"
-// import Menu from "../components/Menu"
 // import { Link } from "gatsby"
 // import Image from "../components/image"
 // import Layout from "../components/layout"
@@ -30,7 +30,7 @@ const IndexPage = () => {
             <div>
               <Layout title="Home">
                 <Home />
-                {/* <Menu /> */}
+                <Menu />
                 <Projects />
                 <ContactForm />
               </Layout>
