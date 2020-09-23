14978aa18704357ede228c64549058ea1482f8ef
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-09-23 12:45:30 +0200

    ADDED EMAIL API

--- api/email.ts
+++ api/email.ts
@@ -1,10 +1,11 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 import sgMail from "@sendgrid/mail"
+import { NowRequest, NowResponse } from "@vercel/node"
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-export default () => {
+export default (request: NowRequest, response: NowResponse) => {
   const msg = {
     to: "giacomo.pasin@gmail.com",
     from: "giacomopasincontact@gmail.com",
@@ -13,4 +14,5 @@ export default () => {
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
   }
   sgMail.send(msg)
+  response.send("hello")
 }
