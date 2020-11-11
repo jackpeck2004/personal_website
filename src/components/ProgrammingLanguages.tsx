49538572119c2039bbc2da7fc7e12a13cd5c8b90
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-11-11 08:45:38 +0100

    modified programming languages

--- src/components/ProgrammingLanguages.tsx
+++ src/components/ProgrammingLanguages.tsx
@@ -1,54 +1,53 @@
-import React from "react"
-import '../styles/ProgrammingLanguages.scss'
+import React from 'react';
+import '../styles/ProgrammingLanguages.scss';
 
 const languages = [
-    {
-        name: "HTML",
-        basic: false,
-    },
-    {
-        name: "CSS",
-        basic: false,
-    },
-    {
-        name: "JavaScript",
-        basic: false,
-    },
-    {
-        name: "Python",
-        basic: false,
-    },
-    {
-        name: "Swift",
-        basic: true,
-    },
-    {
-        name: "C++",
-        basic: true,
-    },
-]
+  {
+    name: 'HTML',
+    basic: false,
+  },
+  {
+    name: 'CSS',
+    basic: false,
+  },
+  {
+    name: 'JavaScript',
+    basic: false,
+  },
+  {
+    name: 'Python',
+    basic: false,
+  },
+  {
+    name: 'Swift',
+    basic: true,
+  },
+  {
+    name: 'Typescript',
+    basic: false,
+  },
+];
 
 const ProgrammingLanguages: React.FC = () => {
-    // if (!isMobile) {
-    return (
-        <div className="ProgrammingLanguages">
-            <h2>Programming Languages I Know</h2>
-            <ul>
-                {languages.map((lang, idx) => {
-                    if (!lang.basic) return <li>{lang.name}</li>
+  // if (!isMobile) {
+  return (
+    <div className="ProgrammingLanguages">
+      <h2>Programming Languages I Know</h2>
+      <ul>
+        {languages.map((lang, idx) => {
+          if (!lang.basic) return <li>{lang.name}</li>;
 
-                    return (
-                        <li key={idx}>
-                            {lang.name}
-                            <br /> (basic)
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
-    // }
+          return (
+            <li key={idx}>
+              {lang.name}
+              <br /> (basic)
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+  // }
+};
 
-}
-
-export default ProgrammingLanguages
+export default ProgrammingLanguages;
