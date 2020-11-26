65f1bd809c1e18c96bf2c535ce04c2994db1fbca
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-11-26 13:14:13 +0100

    build: migrated form yarn to npm
    Switched form yarn to npm

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Giacomo Pasin`,
     description: `Giacomo Pasin personal portfolio website. Discover all the projects made by Giacomo Pasin and interact with them. You can get in conntact with Giacomo Pasin as well thanks to this website.`,
     author: `Giacomo Pasin`,
-    keywords: `Giacomo Pasin, Giacomo, giacomo, Pasin, pasin, gatsby, developer, react, software, website, full-stack, full stack, html, css, javascript, js, typescript, ts`
+    keywords: `Giacomo Pasin, Giacomo, giacomo, Pasin, pasin, gatsby, developer, react, software, website, full-stack, full stack, html, css, javascript, js, typescript, ts`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -37,11 +37,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
