e13264fba795bdb9715e61ff161328f77214112a
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-11-27 16:45:43 +0100

    build(favicon): moved favicon.ico to jpeg

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.jpeg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
