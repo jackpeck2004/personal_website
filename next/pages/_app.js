5e10b6fcfcee40ab3968caba4e0c1fa2aaa75923
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-03-19 14:35:47 +0100

    ran prettier

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css';
-import "../styles/global.css"
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
