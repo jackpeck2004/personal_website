67d9b5b8a9b33b60bfd21e6a8ace00d3ff59d487
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-03-21 16:10:47 +0100

    fixed formatting

--- server/models/project.go
+++ server/models/project.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Project struct {
-	ID				string		`json:"id"`
-	Name			string		`json:"name"`
+	ID				    string		`json:"id"`
+	Name			    string		`json:"name"`
 	Description		string		`json:"description"`
-	Languages		[]string	`json:"languages"`
+	Languages		  []string	`json:"languages"`
 	Frameworks		[]string	`json:"frameworks"`
-	GithubUrl		*string		`json:"githubUrl"`
+	GithubUrl		  *string		`json:"githubUrl"`
 	ProjectUrl		*string		`json:"projectUrl"`
-	CreatedAt		*time.Time	`json:"createdAt"`
-	UpdatedAt		*time.Time	`json:"updatedAt"`
+	CreatedAt		  *time.Time	`json:"createdAt"`
+	UpdatedAt		  *time.Time	`json:"updatedAt"`
 }
