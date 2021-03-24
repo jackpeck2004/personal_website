f82b03d54f3f70bbd0867ba96a0fba4236d9a731
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-03-24 16:51:36 +0100

    added darkmode toggle

--- next/components/topbar.tsx
+++ next/components/topbar.tsx
@@ -3,13 +3,44 @@ import { SITE_NAME } from '../lib/constants';
 //import Scroller from "./scroller"
 
 const Topbar: React.FC = () => {
+  const [darkMode, setDarkMode] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    if (
+      localStorage.theme === 'dark' ||
+      (!'theme' in localStorage &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.querySelector('html').classList.add('dark');
+    } else if (localStorage.theme === 'dark') {
+      document.querySelector('html').classList.add('dark');
+    }
+  }, []);
+
+  const toggleDark: void = () => {
+    let htmlClasses = document.querySelector('html').classList;
+    if (localStorage.theme == 'dark') {
+      htmlClasses.remove('dark');
+      localStorage.removeItem('theme');
+    } else {
+      htmlClasses.add('dark');
+      localStorage.theme = 'dark';
+    }
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
       <div className="h-20 flex items-center">
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between">
           <a href="/" className="uppercase font-semibold text-2xl">
             {SITE_NAME}
           </a>
+          <div>
+            <button onClick={() => toggleDark()}>
+              {darkMode ? 'Light' : 'Dark'}
+            </button>
+          </div>
         </div>
       </div>
       {/*<Scroller />*/}
