834a62e61f13d87da5346f6a4862712a59b68cea
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-11-07 12:28:00 +0100

    Implemented useTheme hook to return the themes

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,14 +6,18 @@ import "bootstrap/dist/css/bootstrap.css";
 import { ThemeProvider } from "@emotion/react";
 import useMediaQuery from "../../lib/hooks/useMediaQuery";
 import constants from "../../lib/constants";
+import useTheme from "../../lib/hooks/useTheme";
 
 const Main = ({ children, router: _router }) => {
   const [isLightTheme, setIsLightTheme] = useState(true);
 
+  const theme = useTheme()
+
   const isDesktop = useMediaQuery(constants.mediaQueries.IS_LARGE);
+
   return (
     <>
-      <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
+      <ThemeProvider theme={isLightTheme ? theme.light : theme.dark}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {/* 
