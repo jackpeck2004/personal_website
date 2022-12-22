3072cd0806c0059c3fc761b58aeeb582fd2d9453
Giacomo Pasin <giacomo.pasin@gmail.com>
2022-12-22 16:21:32 +0100

    Fixed capitalization of text in button

--- src/components/common/download-cv-button.tsx
+++ src/components/common/download-cv-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const DownloadCVButton = () => (
   <Link href="https://drive.google.com/open?id=1eircWWFHYO6Gyx8I9Wsw678kjDVkEMLH&authuser=giacomo%40twin.services&usp=drive_fs">
     <a
-      className="bg-blue-600 text-white text-center py-2 px-4 rounded-lg drop-shadow-2xl lowercase"
+      className="bg-blue-600 text-white text-center py-2 px-4 rounded-lg drop-shadow-2xl lowercase lg:capitalize"
       target="_blank"
     >
       Download my CV
