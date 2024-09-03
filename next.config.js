67a6a6631b10db9c3bca1b7c982d63deadb6fe4f
Giacomo Pasin <giacomo.pasin@gmail.com>
2024-09-03 10:31:15 +0200

    Added md(x) page support

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true
-  }
-};
---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+import createMDX from '@next/mdx'
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true
+  }
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
