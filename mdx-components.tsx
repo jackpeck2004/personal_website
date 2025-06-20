a0c86b97419fbd243ec587807b70a5c95d5c337e
Giacomo Pasin <giacomo.pasin@gmail.com>
2025-06-20 10:29:25 +0200

    Fix: meta title in blog section

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,5 +3,11 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    h1: (props) => <h2 {...props} />,
+    h2: (props) => <h3 {...props} />,
+    h3: (props) => <h4 {...props} />,
+    h4: (props) => <h5 {...props} />,
+    h5: (props) => <h6 {...props} />,
+    h6: (props) => <h6 {...props} />, // Or, for example, <p {...props}><strong>{props.children}</strong></p>
   }
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 import createMDX from '@next/mdx'
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,8 +12,14 @@ const nextConfig = {
 }
  
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-})
+  options: {
+    remarkPlugins: [
+      remarkFrontmatter, // Parses frontmatter
+      [remarkMdxFrontmatter, { name: 'meta' }] // Exports frontmatter as 'meta'
+    ],
+    rehypePlugins: [],
+  },
+});
  
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig)
