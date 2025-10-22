8ec8d3c6b8da71bab9323bb64be8f89d3a89849a
Giacomo Pasin <giacomo.pasin@gmail.com>
2025-10-22 16:11:18 +0200

    added padding on mobile

--- src/app/articles/(posts)/layout.tsx
+++ src/app/articles/(posts)/layout.tsx
@@ -8,7 +8,7 @@ export default function BlogPostContainer({ children }: {children: ReactNode}) {
             <Link href="/articles" className="hover:text-blue-500">All posts</Link>
         </div>
         <main>
-        <article className="container mx-auto mt-8 prose prose-h1:font-serif prose-h1:text-blue-600 prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-serif prose-lg">
+        <article className="container mx-auto mt-8 prose prose-h1:font-serif prose-h1:text-blue-600 prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-serif prose-lg px-4">
             {children}
         </article>
         </main>
